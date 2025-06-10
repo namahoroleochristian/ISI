815a59d5048f508e578eab88cee59d1b24b6b34c
Namahoro Leo Christian <126883714+namahoroleochristian@users.noreply.github.com>
2025-06-10 08:26:35 +0200

    Update brand name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Logo */}
-          <div className="text-2xl font-bold text-[#BCA14F]">MyBrand</div>
+          <div className="text-2xl font-bold text-[#BCA14F]">INTARE SW</div>
 
           {/* Desktop Menu */}
           <div className="hidden md:flex space-x-6">
